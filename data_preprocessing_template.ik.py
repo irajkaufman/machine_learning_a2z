b7c61fd0bcb4b0094e356c0c4fa067f5279ef3f6
Ira Kaufman <irajkaufman@Iras-iMac.local>
2018-05-26 15:34:04 -0700

    Adding python file for Data Pre-processing (don't need one for r, since the equivalent libraries for it are already there, automatically).

---
+++ data_preprocessing_template.ik.py
@@ -0,0 +1,13 @@
+# Data Preprocessing
+
+# Importing the libraries
+
+"""
+Created on Sat May 26 15:24:48 2018
+
+@author: irajkaufman
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
