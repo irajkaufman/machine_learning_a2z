26f015d3ff76b90dcdc41ac35aadbf4e55704b7f
Ira Kaufman <irajkaufman@Iras-iMac.local>
2018-05-26 23:03:32 -0700

    Modified python file for Data Pre-processing, Added r file for Data Preprocessing (both to import the dataset), Added Data (csv) File

--- data_preprocessing_template.ik.py
+++ data_preprocessing_template.ik.py
@@ -1,6 +1,5 @@
 # Data Preprocessing
 
-# Importing the libraries
 
 """
 Created on Sat May 26 15:24:48 2018
@@ -8,6 +7,12 @@ Created on Sat May 26 15:24:48 2018
 @author: irajkaufman
 """
 
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
